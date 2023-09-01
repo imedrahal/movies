07a2f674eac01dc72bb9956deee0ebc1f1c6659d
imédrahal <imedrahal1996@gmail.com>
2023-09-01 14:39:37 +0100

    first srtting

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="movienight-wrapper">
+              <div className="movienight">MOVIENIGHT</div>
+            </div>
+    </div>
+  );
+}
+
+export default App;
